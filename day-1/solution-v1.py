7abaffe36b03860ed1051af1268d22172e4d8587
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-12-02 22:07:41 +0100

    Clean up

--- day-1/solution-v1.py
+++ day-1/solution-v1.py
@@ -9,29 +9,29 @@ def solution():
     with open("input.txt") as f:
         lines = f.readlines()
 
-        # Add all lines the corresponding list
-        c = len(lines)
-        for line in lines:
-            left.append(int(line.split()[0]))
-            right.append(int(line.split()[1]))
+    # Add all lines the corresponding list
+    c = len(lines)
+    for line in lines:
+        left.append(int(line.split()[0]))
+        right.append(int(line.split()[1]))
 
-        for _ in range(c):
-            # Smallest element of each list (bigger than last time)
-            left_smallest = min(left)
-            right_smallest = min(right)
+    for _ in range(c):
+        # Smallest element of each list (bigger than last time)
+        left_smallest = min(left)
+        right_smallest = min(right)
 
-            # Make absolute difference between indexes
-            data.append(abs(left_smallest - right_smallest))
+        # Make absolute difference between indexes
+        data.append(abs(left_smallest - right_smallest))
 
-            # Index of smallest element
-            left_index = left.index(left_smallest)
-            right_index = right.index(right_smallest)
+        # Index of smallest element
+        left_index = left.index(left_smallest)
+        right_index = right.index(right_smallest)
 
-            # Remove from in list
-            left[left_index] = math.inf
-            right[right_index] = math.inf
+        # Remove from in list
+        left[left_index] = math.inf
+        right[right_index] = math.inf
 
-        return sum(data)
+    return sum(data)
 
 
 if __name__ == "__main__":
