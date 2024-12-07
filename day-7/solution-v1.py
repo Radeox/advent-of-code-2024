a53768b351a6b5fd0620282063b9b83fa65447f4
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-12-07 15:42:58 +0100

    Day 7 solution

---
+++ day-7/solution-v1.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+
+def solution():
+    solution = 0
+    equations = {}
+
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        result = int(line.strip().split(": ")[0])
+        equations[result] = [int(i) for i in line.strip().split(": ")[1].split(" ")]
+
+    for eq in equations:
+        # Get all possibile operations
+        for ops in product(["+", "*"], repeat=len(equations[eq]) - 1):
+            # Make the equation
+            result = equations[eq][0]
+            for i, num in enumerate(equations[eq][1:]):
+                result = eval(f"{result} {ops[i]} {num}")
+
+            if result == eq:
+                solution += eq
+                break
+
+    return solution
+
+
+if __name__ == "__main__":
+    print(f"Solution: {solution()}")
